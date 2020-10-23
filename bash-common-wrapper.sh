6ffb0e452eae7a8e705095b009513914b04a45f7
Baylen Orr <EZpsychedelic@users.noreply.github.com>
2020-10-23 11:09:22 -0600

    Create bash-common-wrapper.sh

---
+++ bash-common-wrapper.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## Error wrapper
+error ()
+{
+  echo >&2 `tput setaf 1`"[-] FAILED: ${*}"`tput sgr0`
+  exit 78
+}
+
+## Check root privilage wrapper
+check_root ()
+{
+  if [ $EUID -ne 0 ]; then
+    error "User must be root!"
+  fi
+}
+
+## Make a Directory if it doesn't exist
+make_newDir ()
+{
+  if [ ! -d "$newDir ]; then
+    mkdir -pv /mnt/$newDir
+    echo "[+] $newDir created"
+  fi
+}
+## If previous return code does not equal 0
+if [ $? -ne 0 ]; then
+do somthing else
+## If no arguments are passed send an error message and exit
+parse_args ()
+{
+  if [[ $# -eq 0 ]]; then
+  echo "Usage: Some usage mesage..."
+  exit 1
+  fi
+}
