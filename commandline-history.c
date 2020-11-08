1ecae942b5a1ae850bbaa591e4e5f5573b4e10be
Baylen Orr <EZpsychedelic@users.noreply.github.com>
2020-11-07 17:09:04 -0700

    Create commandline-history.c

---
+++ commandline-history.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char argv *[]) {
+	int i;
+	
+	for(i = 0; i < argc; i++;)
+		
+	//needed calls (pg 83):
+	var = open(logfile, O_WRONLY|O_CREATE|O_APPEND, S_ISUSR|S_IWUSR); //Opens/Creates and appends log file
+	write(
+		printf("[+] %s\n"; argv[i]);
+	return(0);
+}
